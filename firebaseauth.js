7fe3da4effb02f447aa3e24e49c59fb9c35118f9
SiD-77x <siddeshsable001@gmail.com>
2024-09-30 19:36:12 +0530

    Create firebaseauth.js

---
+++ firebaseauth.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXrlcUv7DgVuTzxhGC64BNiEYS5u2uVX0",
+    authDomain: "login-form-8de2c.firebaseapp.com",
+    projectId: "login-form-8de2c",
+    storageBucket: "login-form-8de2c.appspot.com",
+    messagingSenderId: "203936392599",
+    appId: "1:203936392599:web:a1b22bf733290a20b190d2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
